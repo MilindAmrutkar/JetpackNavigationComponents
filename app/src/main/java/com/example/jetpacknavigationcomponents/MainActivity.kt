e2cf7f4d76372dc2a1671b88f5c06fae0a0d49aa
Milind Prakash Amrutkar <milindprakashamrutkar@Milinds-MacBook-Pro.local>
2025-06-16 06:30:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jetpacknavigationcomponents/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.jetpacknavigationcomponents
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpacknavigationcomponents.ui.theme.JetpackNavigationComponentsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetpackNavigationComponentsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Milind Welcome to your new macbook",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackNavigationComponentsTheme {
+        Greeting("Android")
+    }
+}
